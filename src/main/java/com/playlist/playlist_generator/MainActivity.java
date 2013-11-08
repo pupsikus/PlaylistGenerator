8bb064d8ce83a9e3b4a91526f388e1094f5e94f9
pupsikus <voronov.a.g@gmail.com>
2013-11-08 12:35:09 +0200

    Create Playlist in Unicode
    Function Generate now generates playlist, but file invisible for media
    players.

--- src/main/java/com/playlist/playlist_generator/MainActivity.java
+++ src/main/java/com/playlist/playlist_generator/MainActivity.java
@@ -3,6 +3,7 @@ package com.playlist.playlist_generator;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -13,8 +14,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -102,29 +110,69 @@ public class MainActivity extends ListActivity implements OnClickListener {
     private void PLGenerator(){
         String ItemPath;
         trackNames = new ArrayList<String>();
+        ArrayList<ArrayList<String>> dirFiles = new ArrayList<ArrayList<String>>();
         for(int i=0;i < MusicOptionsList.size();i++){
+            dirFiles.add(new ArrayList<String>());
             for(int j=0; j < MusicOptionsList.get(i).getOptionPathSize(); j++){
-                //MusicOptionsList.get(i);
                 ItemPath = MusicOptionsList.get(i).getPath(j);
-                addTracks(getTracks(ItemPath));
+                addTracks(getTracks(ItemPath, dirFiles.get(i)));
             }
-
+            Collections.shuffle(dirFiles.get(i));
         }
+        CreatePList(dirFiles);
         random = new Random();
 
     }
 
-    //Generate a String Array that represents all of the files found
-    private ArrayList<String > getTracks(String MusicFolderPath){
-        path = new File(MusicFolderPath);
-        String[] temp = path.list();
-        File[] files = path.listFiles();
-        ArrayList<String> dirFiles = new ArrayList<String>();
-        for(File file : files){
-            dirFiles.add(file.getAbsolutePath());
-        }
+    private void CreatePList(ArrayList<ArrayList<String>> OptionsFilesList){
+        //Try code and catch exceptions
+        try {
+           /* We have to use the openFileOutput()-method
+           * We chose MODE_WORLD_READABLE, because
+           * we have nothing to hide in our file */
 
-        return dirFiles;
+            File file = new File(Environment.getExternalStorageDirectory() + "/Music", "Test.m3u");
+            PrintWriter writer = new PrintWriter(file, "Unicode");
+            //FileOutputStream osw = new FileOutputStream(file);
+             //FileOutputStream fOut = openFileOutput("Test.m3u",MODE_WORLD_WRITEABLE);
+            //OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+            // Write the string to the file
+            for (int i=0;i<OptionsFilesList.size();i++){
+                for(int j=0;j<OptionsFilesList.get(i).size();j++){
+                    //osw.write(OptionsFilesList.get(i).get(j).toString());
+                    //osw.write(OptionsFilesList.get(i).get(j).getBytes());
+                    //osw.write('\n');
+                    writer.println(OptionsFilesList.get(i).get(j).toString()+"\r");
+                }
+            }
+           /* ensure that everything is
+            * really written out and close */
+            //osw.flush();
+            //osw.close();
+            writer.flush();
+            writer.close();
+        }
+        catch (IOException ioe)
+        {ioe.printStackTrace();}
+    }
+
+    //Generate a String Array that represents all of the files found
+    private ArrayList<String> getTracks(String directoryName, ArrayList<String> files) {
+        File directory = new File(directoryName);
+
+        // get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList) {
+            if (file.isFile()) {
+                if(trackChecker(file.getName().toString())){
+                    files.add(file.getAbsolutePath());
+                }
+            } else if (file.isDirectory()) {
+                getTracks(file.getAbsolutePath(), files);
+            }
+        }
+        return files;
     }
 
     //Adds the playable files to the trackNames List
